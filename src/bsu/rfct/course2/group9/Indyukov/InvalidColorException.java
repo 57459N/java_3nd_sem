603ec7dbdd881abfa1b70169384710a2fb910564
Stanislau Indyukou <rct.indyukov@bsu.by>
2022-11-03 20:51:20 +0300

    prepared to 3rd task

--- src/bsu/rfct/course2/group9/Indyukov/InvalidColorException.java
+++
@@ -1,7 +0,0 @@
-package bsu.rfct.course2.group9.Indyukov;
-
-public class InvalidColorException extends Exception{
-    public InvalidColorException(String message){
-        super(message);
-    }
-}
--- src/bsu/rfct/course2/group9/Indyukov/InvalidIcingException.java
+++
@@ -1,7 +0,0 @@
-package bsu.rfct.course2.group9.Indyukov;
-
-public class InvalidIcingException extends Exception{
-    public InvalidIcingException(String message){
-        super(message);
-    }
-}
